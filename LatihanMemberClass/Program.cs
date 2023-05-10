89c055efbe6149c806ca76fc965ed2ff150d8cef
dhiiim <dhimasprasetya517@students.amikom.ac.id>
2023-05-10 12:35:21 +0700

    LatihanMemberClass

---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            planeticket ticket1 = new planeticket();
+            planeticket ticket2 = new planeticket();
+
+            ticket1.origin = "JOGJA";
+            ticket1.destination = "PKU";
+            ticket1.cost = 100;
+            ticket1.currency = "USD";
+
+            ticket2.origin = "JKT";
+            ticket2.destination = "PKU";
+            ticket2.cost = 750000;
+            ticket2.currency = "IDR";
+
+            ticket1.PrintSummary();
+            Console.WriteLine();
+            ticket2.PrintSummary();
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+}
---
+++ LatihanMemberClass/planeticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class planeticket
+    {
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public int cost { get; set; }
+        public string currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("origin: {0}", origin);
+            Console.WriteLine("destination: {0}", destination);
+            Console.WriteLine("cost: {0} {1}", cost, currency);
+
+            if (currency == "IDR")
+            {
+                cost = cost / 15000;
+                Console.WriteLine("convert to dollar: {0}", cost);
+            }
+            else if (currency == "USD")
+            {
+                cost = cost * 15000;
+                Console.WriteLine("convert to rupiah: {0}", cost);
+            }
+
+
+        }
+    }
+}
